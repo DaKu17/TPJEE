78f0b7fac462b434b9105f924e151045c76ae54e
DaKu17 <Salma.Elhourrach@emsi-edu.ma>
2025-03-19 23:43:22 +0000

    Add files via upload

---
+++ TP1/src/main/java/jakarta/servlet/Imc.java
@@ -0,0 +1,16 @@
+package jakarta.servlet;
+
+public class Imc {
+	    private double taille;
+	    private double poids;
+
+	    public Imc(double taille, double poids) {
+	        this.taille = taille;
+	        this.poids = poids;
+	    }
+
+	    public double calcul() {
+	        return poids / (taille * taille);
+	    }
+	}
+
